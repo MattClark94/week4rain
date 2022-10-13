7ac468fead5fc881034521955ca1a196b354b8d0
Matt.Clark <mc1556@student.aru.ac.uk>
2022-10-13 10:04:55 +0100

    new stuff

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿int[] rainfall= new int[12]; 
+int average; 
+
+rainfall[0] = 56; 
+rainfall[1] = 87; 
+rainfall[2] = 78; 
+rainfall[3] = 23; 
+rainfall[4] = 21; 
+rainfall[5] = 12; 
+rainfall[6] = 1; 
+rainfall[7] = 98; 
+rainfall[8] = 90; 
+rainfall[9] = 67; 
+rainfall[10] = 54; 
+rainfall[11] = 44; 
+rainfall[12] = 88; 
+
+
+average = rainfall[3] + rainfall[1]; 
+
+Console.WriteLine("The rainfall for 2 months is "+ average);
+
+Console.ReadKey(); 
