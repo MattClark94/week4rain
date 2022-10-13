ba47d7cdada52e94a8145c3c93a5df42971511f7
Matt.Clark <mc1556@student.aru.ac.uk>
2022-10-13 11:33:07 +0100

    adding the maths part

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿int[] rainfall= new int[12]; 
-
+int total = 0; 
 string[] month = new string[12] { "Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"}; 
 
 for (int i = 0; i < rainfall.Length; i++)
@@ -8,12 +8,17 @@ for (int i = 0; i < rainfall.Length; i++)
     rainfall[i] = Convert.ToInt32(Console.ReadLine()); 
 }
 
+
+foreach (int amount in rainfall)
+{
+     total = total + amount;
+}
+
 Console.Clear(); 
 
 for (int i = 0; i < rainfall.Length; i++)
 {
-    Console.WriteLine(" For the month of" + month[i] + "=" + rainfall[i]); 
+    Console.WriteLine(" For the month of " + month[i] + " = " + rainfall[i]); 
 }
-
 
 Console.ReadKey(); 
