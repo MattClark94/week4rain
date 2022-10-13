bbdf8c5e39246f9712ba85b926385740117d3032
Matt.Clark <mc1556@student.aru.ac.uk>
2022-10-13 11:07:04 +0100

    Changes

--- Program.cs
+++ Program.cs
@@ -1,23 +1,19 @@
 ﻿int[] rainfall= new int[12]; 
-int average; 
 
-rainfall[0] = 56; 
-rainfall[1] = 87; 
-rainfall[2] = 78; 
-rainfall[3] = 23; 
-rainfall[4] = 21; 
-rainfall[5] = 12; 
-rainfall[6] = 1; 
-rainfall[7] = 98; 
-rainfall[8] = 90; 
-rainfall[9] = 67; 
-rainfall[10] = 54; 
-rainfall[11] = 44; 
-rainfall[12] = 88; 
+for (int i = 0; i < rainfall.Length; i++)
+{ 
+    Console.WriteLine(" Enter amount " + i + ": "); 
+    rainfall[i] = Convert.ToInt32(Console.ReadLine()); 
+}
+
+Console.Clear(); 
+
+for (int i = 0; i < rainfall.Length; i++)
+{
+    Console.WriteLine("rainFall" + i + "=" +rainfall[i]); 
+}
 
 
-average = rainfall[3] + rainfall[1]; 
 
-Console.WriteLine("The rainfall for 2 months is "+ average);
 
 Console.ReadKey(); 
